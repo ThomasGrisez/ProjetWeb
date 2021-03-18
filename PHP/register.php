c2cb66f76c93ce202b81c8742c8a912a29c700fb
Thomas <thomas.grisez@edu.ece.fr>
2021-03-18 15:12:36 +0100

    Registration Page Done !

--- PHP/register.php
+++ PHP/register.php
@@ -11,18 +11,20 @@
     <?php include 'header.php'?>
 
     <?php
-        require 'config.php';
+        //Database
+        $mysqli = new mysqli('127.0.0.1','root', '', 'fitnet', NULL) or die("Connect failed");
 
         if(isset($_POST['inscription'])){
             $firstName = htmlspecialchars($_POST['fname']);
             $lastName = htmlspecialchars($_POST['lname']);
             $mail = htmlspecialchars($_POST['email']);
             $mail2 = htmlspecialchars($_POST['email2']);
-            $pw = sha1($_POST['password']);
-            $pw2 = sha1($_POST['password2']);
+            $pw = $_POST['password'];
+            $pw2 = $_POST['password2'];
             $type = $_POST['type'];
+            unset($msg);
 
-            if(!empty($_POST['fname']) &&  !empty($_POST['lname']) && !empty($_POST['email']) && !empty($_POST['email2']) && !empty($_POST['password']) && !empty($_POST['password2'])){
+            if(!empty($_POST['fname']) &&  !empty($_POST['lname']) && !empty($_POST['email']) && !empty($_POST['email2']) && !empty($_POST['password']) && !empty($_POST['password2']) && !empty($_POST['type'])){
                 if($mail == $mail2){
                     if(filter_var($mail, FILTER_VALIDATE_EMAIL)){
                         $result1 = $mysqli->query("SELECT * FROM buyer WHERE email = '$mail'");
@@ -30,7 +32,11 @@
                         $result3 = $mysqli->query("SELECT * FROM administrator WHERE email = '$mail'");
                         if($result1->num_rows == 0 && $result2->num_rows == 0 && $result3->num_rows == 0){
                             if($pw == $pw2){
-                                $myslqi->query("INSERT INTO ")
+                                if($type == "buyer")
+                                    {$mysqli->query("INSERT INTO `buyer` (`id`, `last_name`, `first_name`, `address`, `email`, `password`, `shopping_cart`) VALUES(NULL,'$lastName','$firstName', '','$mail','$pw','')");}
+                                if($type == "seller")
+                                    {$mysqli->query("INSERT INTO `seller`(`id`, `last_name`, `first_name`, `email`, `password`, `photo`, `favorite_background`, `list_items`) VALUES(NULL,'$lastName','$firstName','$mail','$pw','','','')");}
+                                $msg = "Account created ! <a href=\"login.php\">Log in</a>";
                             }else{
                                 $msg = "The passwords are different !";
                             }
@@ -44,10 +50,7 @@
                     $msg = "Your email addresses are different !";
                 }
             }
-
         }
-
-
     ?>
 
     <section class="main">
@@ -58,13 +61,13 @@
                 <input type="text" name="fname" id="fname" placeholder="First name">
                 <input type="text" name="email" id="email" placeholder="Email">
                 <input type="text" name="email2" id="email2" placeholder="Confirm your Email">
-                <input type="text" name="password" id="password" placeholder="Password">
-                <input type="text" name="password2" id="password2" placeholder="Confirm your Password">
+                <input type="password" name="password" id="password" placeholder="Password">
+                <input type="password" name="password2" id="password2" placeholder="Confirm your Password">
 
                 
                 <div class="radios">
                     <label for="buyer">Buyer</label>               
-                        <input type="radio" name="type" value="buyer">
+                        <input type="radio" name="type" value="buyer" id="buyer">
 
                     <label for="seller">Seller</label>
                         <input type="radio" name="type" value="seller">
@@ -73,10 +76,19 @@
                 <div id="button-register">
                     <input type="submit" name="inscription" value="Confirm">
                 </div>
+                
+                <?php
+                if(isset($msg)) {
+                    echo '<font color="red">'.$msg."</font>";
+                }
+                ?>
             </form>
             
         </div>
     </section>
+
+    <script>document.getElementById("buyer").checked = true;</script>
+
 
     <?php include 'footer.php'?>
 </body>
