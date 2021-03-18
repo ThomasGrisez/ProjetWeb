e6a8d3688af07db0d6405a0a4c5d97200b4e1919
Thomas Grisez <thomas.grisez@edu.ece.fr>
2021-03-18 14:05:12 +0100

    avancée register

---
+++ PHP/config.php
@@ -0,0 +1,5 @@
+<?php
+//Database
+$mysqli = new mysqli('127.0.0.1','root', '', 'fitnet', NULL) or die("Connect failed");
+
+?>
--- PHP/register.php
+++ PHP/register.php
@@ -4,13 +4,51 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fit.net | Login</title>
-	<script src="http://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="../JS/index.js"></script>
 	<link rel="icon" type="image/png" href="../Images/favicon.png" />
     <link rel="stylesheet" type="text/css" href="../CSS/register.css">
 </head>
 <body>
     <?php include 'header.php'?>
+
+    <?php
+        require 'config.php';
+
+        if(isset($_POST['inscription'])){
+            $firstName = htmlspecialchars($_POST['fname']);
+            $lastName = htmlspecialchars($_POST['lname']);
+            $mail = htmlspecialchars($_POST['email']);
+            $mail2 = htmlspecialchars($_POST['email2']);
+            $pw = sha1($_POST['password']);
+            $pw2 = sha1($_POST['password2']);
+            $type = $_POST['type'];
+
+            if(!empty($_POST['fname']) &&  !empty($_POST['lname']) && !empty($_POST['email']) && !empty($_POST['email2']) && !empty($_POST['password']) && !empty($_POST['password2'])){
+                if($mail == $mail2){
+                    if(filter_var($mail, FILTER_VALIDATE_EMAIL)){
+                        $result1 = $mysqli->query("SELECT * FROM buyer WHERE email = '$mail'");
+                        $result2 = $mysqli->query("SELECT * FROM seller WHERE email = '$mail'");
+                        $result3 = $mysqli->query("SELECT * FROM administrator WHERE email = '$mail'");
+                        if($result1->num_rows == 0 && $result2->num_rows == 0 && $result3->num_rows == 0){
+                            if($pw == $pw2){
+                                $myslqi->query("INSERT INTO ")
+                            }else{
+                                $msg = "The passwords are different !";
+                            }
+                        }else{
+                            $msg = "This email adress already exists !";
+                        } 
+                    }else{
+                        $msg = "The email address is not valid -> name@example.com";
+                    }
+                }else{
+                    $msg = "Your email addresses are different !";
+                }
+            }
+
+        }
+
+
+    ?>
 
     <section class="main">
         <div class="content-register">
@@ -19,7 +57,10 @@
                 <input type="text" name="lname" id="lname" placeholder="Last name">
                 <input type="text" name="fname" id="fname" placeholder="First name">
                 <input type="text" name="email" id="email" placeholder="Email">
+                <input type="text" name="email2" id="email2" placeholder="Confirm your Email">
                 <input type="text" name="password" id="password" placeholder="Password">
+                <input type="text" name="password2" id="password2" placeholder="Confirm your Password">
+
                 
                 <div class="radios">
                     <label for="buyer">Buyer</label>               
@@ -30,7 +71,7 @@
                 </div>
 
                 <div id="button-register">
-                    <input type="submit" value="Confirm">
+                    <input type="submit" name="inscription" value="Confirm">
                 </div>
             </form>
             
