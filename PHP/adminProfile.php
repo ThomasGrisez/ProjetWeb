60440638c58f138b7b4aaca05116d285a87c41be
Faritaz <thibaultgautierl@gmail.com>
2021-04-03 19:53:57 +0200

    Qucik Update CSS for Admin Profile

--- PHP/adminProfile.php
+++ PHP/adminProfile.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fit.net | Profile</title>
 	<link rel="icon" type="image/png" href="../Images/favicon.png" />
+     <?php include '../CSS/profileCSS.php' ?>
 </head>
 <body>
     <?php include 'header.php'?>
@@ -14,12 +15,11 @@
     ?>
     
     <div class="profile" align="center">
-        <h2>Administrator <?php echo " : ".$_SESSION['id']; ?></h2>
-        Mail = <?php echo $_SESSION['email']; ?>
-        <br>
-        Password = <?php echo $_SESSION['password']; ?>
+        <h2 class="main_title">Administrator access<?php echo " : ".$_SESSION['id']; ?></h2>
+        <p><span class="lis_of_information_title">Mail :</span> <?php echo $_SESSION['email']; ?></p>
+        <p><span class="lis_of_information_title">Password :</span>  <?php echo $_SESSION['password']; ?></p>
         <br><br>
-        Seller(s):<br>
+         <h3 style='font-size : 20px;'><u><em>Seller(s):</em></u></h3>
         <?php 
             $query = $mysqli->query("SELECT last_name, first_name, id FROM seller");
             if($query->num_rows > 0){
@@ -29,7 +29,7 @@
             }
         ?>
         <br>
-        Buyer(s):<br>
+        <h3 style='font-size : 20px;'><u><em>Buyer(s):</em></u></h3>
         <?php 
             $query = $mysqli->query("SELECT last_name, first_name, id FROM buyer");
             if($query->num_rows > 0){
@@ -38,9 +38,15 @@
                 }
             }
         ?>
-        <br>
-        <a href="editAdmin.php">Edit my profile</a>
-        <a href="logout.php">Log out</a>
+        <br><br><br>
+        <div class="main_aspect_of_profile_bloc" id="bloc_button_buyer_profil">
+            <div class="button_list_buyer_profil">  
+                <a href="editAdmin.php"><button class="button_of_informations"  id="edit_information_buyer">Edit my profile</button></a>
+            </div>
+            <div class="button_list_buyer_profil">  
+                <a href="logout.php"><button class="button_of_informations"  id="logout_information_buyer">Log out</button></a>
+            </div>
+        </div>
     </div>
     <?php include 'footer.php'?>
 </body>
